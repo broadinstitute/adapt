b4e6c4a8f3a662cf0a831c8546a0acdd093aeb0d
Hayden Metsky <hmetsky@gmail.com>
2017-05-05 16:14:54 -0400

    Add methods for working with a sequence alignment

---
+++ probedesign/alignment.py
@@ -0,0 +1,225 @@
+"""Structure(s) and functions for working with alignments of sequences.
+"""
+
+import logging
+
+from probedesign.utils import probe
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+logger = logging.getLogger(__name__)
+
+
+class Alignment:
+    """Immutable collection of sequences that have been aligned.
+
+    This stores sequences in column-major order, which should make it more
+    efficient to extract pieces of the alignment by position and to generate
+    consensus sequences.
+    """
+
+    def __init__(self, seqs):
+        """
+        Args:
+            seqs: list of str representing an alignment in column-major order
+                (i.e., seqs[i] is a string giving the bases in the sequences
+                at the i'th position of the alignment; it is not the i'th sequence)
+        """
+        self.seq_length = len(seqs)
+        self.num_sequences = len(seqs[0])
+        for s in seqs:
+            assert len(s) == self.num_sequences
+
+        self.seqs = seqs
+
+    def extract_range(self, pos_start, pos_end):
+        """Extract range of positions from alignment.
+
+        Args:
+            pos_start: start position of extraction (inclusive)
+            pos_end: end position of extraction (exclusive)
+
+        Returns:
+            object of type Alignment including only the specified range
+        """
+        return Alignment(self.seqs[pos_start:pos_end])
+
+    def has_indel(self):
+        """Determine whether there is an indel in the alignment.
+
+        Returns:
+            True iff there is an indel in the alignment
+        """
+        for s in self.seqs:
+            if '-' in s:
+                return True
+        return False
+
+    def construct_probe(self, start, probe_length, seqs_to_consider, mismatches):
+        """Construct a single probe to target a set of sequences in the alignment.
+
+        This constructs a probe to target sequence within the range [start,
+        start+probe_length]. It only considers the sequences with indices given in
+        seqs_to_consider.
+
+        Args:
+            start: start position in alignment at which to target
+            probe_length: length of the probe
+            seqs_to_consider: collection of indices of sequences to use when
+                constructing the probe
+            mismatches: threshold on number of mismatches for determining whether
+                a probe would hybridize to a target sequence
+
+        Returns:
+            tuple (x, y) where:
+                x is the sequence of the constructed probe
+                y is a list of indices of sequences (a subset of
+                    seqs_to_consider) to which the probe x will hybridize
+            (Note that it is possible that x binds to no sequences and that
+            y will be empty.)
+        """
+        assert start + probe_length <= self.seq_length
+        assert len(seqs_to_consider) > 0
+
+        aln_for_probe = self.extract_range(start, start + probe_length)
+
+        # If this region in the alignment has an indel, do not attempt to
+        # construct a probe that covers it (even if the alignment formed by just
+        # the sequences in seqs_to_consider do not have an indel)
+        if aln_for_probe.has_indel():
+            raise CannotConstructProbeError("Region in alignment has indel")
+
+        seqs_to_consider = sorted(list(seqs_to_consider))
+
+        # First construct the optimal probe to cover the sequences. This would be
+        # a string x that maximizes the number of sequences s_i such that x and
+        # s_i are equal to within 'mismatches' mismatches; it's called the "max
+        # close string" or "close to most strings" problem. For simplicity, let's
+        # assume for now that the optimal probe is just the consensus sequence.
+        consensus = aln_for_probe.determine_consensus_sequence(seqs_to_consider)
+        prb = consensus
+
+        # If all that exists at a position in the alignment is 'N', then do
+        # not attempt to cover the sequences because we do not know which
+        # base to put in the probe at that position. In this case, the
+        # consensus will have 'N' at that position.
+        if 'N' in prb:
+            raise CannotConstructProbeError("A position has all 'N'")
+
+        seq_rows = aln_for_probe.make_list_of_seqs(seqs_to_consider)
+        def determine_binding_seqs(prb_sequence):
+            binding_seqs = []
+            for seq_idx, seq in zip(seqs_to_consider, seq_rows):
+                if probe.probe_binds(prb_sequence, seq, mismatches):
+                    binding_seqs += [seq_idx]
+            return binding_seqs
+
+        binding_seqs = determine_binding_seqs(prb)
+
+        # It's possible that the consensus sequence (probe) does not bind to
+        # any of the sequences. In this case, simply select the first
+        # sequence from seq_row that has no ambiguity and make this the probe;
+        # this is guaranteed to have at least one binding sequence (itself)
+        if len(binding_seqs) == 0:
+            for s in seq_rows:
+                if sum(s.count(c) for c in ['A', 'T', 'C', 'G']) == len(s):
+                    # s has no ambiguity and is a suitable probe
+                    prb = s
+                    binding_seqs = determine_binding_seqs(prb)
+                    break
+            # If it made it here, then all of the sequences have ambiguity
+            # (so none are suitable probes); prb will remain the consensus and
+            # binding_seqs will still be empty
+
+        return (prb, binding_seqs)
+
+    def make_list_of_seqs(self, seqs_to_consider=None):
+        """Construct list of sequences from the alignment.
+
+        Args:
+            seqs_to_consider: collection of indices of sequences to use (if None,
+                use all)
+
+        Returns:
+            list of str giving the sequences in the alignment
+        """
+        if seqs_to_consider is None:
+            seqs_to_consider = range(self.num_sequences)
+        return [''.join(self.seqs[j][i] for j in range(self.seq_length))
+                for i in seqs_to_consider]
+
+    def determine_consensus_sequence(self, seqs_to_consider=None):
+        """Determine consensus sequence from the alignment.
+
+        At each position, the consensus is the most common allele even if it is
+        not the majority; the consensus will not be an ambiguity code (except N
+        if all bases are N). Ties are broken arbitrarily.
+
+        This ignores 'N' bases at each position, determining the consensus from
+        the other (non-'N') bases. An 'N' will only be output in the consensus
+        if all bases at a position are 'N'.
+
+        Args:
+            seqs_to_consider: collection of indices of sequences to use (if None,
+                use all)
+
+        Returns:
+            str representing the consensus of the alignment
+        """
+        if seqs_to_consider is None:
+            seqs_to_consider = range(self.num_sequences)
+
+        consensus = ''
+        for i in range(self.seq_length):
+            counts = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+            for b in [self.seqs[i][j] for j in seqs_to_consider]:
+                if b in counts:
+                    counts[b] += 1
+                elif b == 'N':
+                    # skip N
+                    continue
+                elif b in probe.FASTA_CODES:
+                    for c in probe.FASTA_CODES[b]:
+                        counts[c] += 1.0 / len(probe.FASTA_CODES[b])
+                else:
+                    raise ValueError("Unknown base call %s" % b)
+            max_base = max(counts, key=counts.get)
+            if counts[max_base] == 0:
+                consensus += 'N'
+            else:
+                consensus += max_base
+
+        return consensus
+
+    @staticmethod
+    def from_list_of_seqs(seqs):
+        """Construct a Alignment from aligned list of sequences.
+
+        If seqs is stored in row-major order, this converts to column-major order
+        and creates a Alignment object.
+
+        Args:
+            seqs: list of str, all the same length, representing an alignment;
+                seqs[i] is the i'th sequence
+
+        Returns:
+            object of type Alignment
+        """
+        num_sequences = len(seqs)
+        seq_length = len(seqs[0])
+        for s in seqs:
+            if len(s) != seq_length:
+                raise ValueError("Sequences must be the same length")
+
+        seqs_col = ['' for _ in range(seq_length)]
+        for j in range(seq_length):
+            seqs_col[j] = ''.join(seqs[i][j] for i in range(num_sequences))
+
+        return Alignment(seqs_col)
+
+
+class CannotConstructProbeError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
---
+++ probedesign/tests/test_alignment.py
@@ -0,0 +1,115 @@
+"""Tests for alignment module.
+"""
+
+import unittest
+
+from probedesign import alignment
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+
+class TestAlignment(unittest.TestCase):
+    """Tests methods in the Alignment class.
+    """
+
+    def setUp(self):
+        self.a_seqs = ['ATCGAA', 'ATCGAT', 'AYCGAA', 'AYCGAT', 'AGCGAA']
+        self.a = alignment.Alignment.from_list_of_seqs(self.a_seqs)
+
+        self.b_seqs = ['ATCGAA', 'ATNNAT', 'ATCGNN', 'ATNNAT', 'ATNNAC']
+        self.b = alignment.Alignment.from_list_of_seqs(self.b_seqs)
+
+        self.c_seqs = ['ATCGAA', 'ATC-AA']
+        self.c = alignment.Alignment.from_list_of_seqs(self.c_seqs)
+
+    def test_make_list_of_seqs_a(self):
+        self.assertEqual(self.a.make_list_of_seqs(),
+                         self.a_seqs)
+        self.assertEqual(self.a.make_list_of_seqs([0]),
+                         ['ATCGAA'])
+        self.assertEqual(self.a.make_list_of_seqs([0,3]),
+                         ['ATCGAA', 'AYCGAT'])
+
+    def test_make_list_of_seqs_b(self):
+        self.assertEqual(self.b.make_list_of_seqs(),
+                         self.b_seqs)
+
+    def test_make_list_of_seqs_c(self):
+        self.assertEqual(self.c.make_list_of_seqs(),
+                         self.c_seqs)
+
+    def test_determine_consensus_sequence_a(self):
+        self.assertEqual(self.a.determine_consensus_sequence(), 'ATCGAA')
+        self.assertEqual(self.a.determine_consensus_sequence([0]), 'ATCGAA')
+        self.assertIn(self.a.determine_consensus_sequence([2]),
+                      ['ATCGAA', 'ACCGAA'])
+        self.assertIn(self.a.determine_consensus_sequence([0,1]),
+                      ['ATCGAA', 'ATCGAT'])
+        self.assertIn(self.a.determine_consensus_sequence([1,2]),
+                      ['ATCGAT', 'ATCGAA'])
+
+    def test_determine_consensus_sequence_b(self):
+        self.assertEqual(self.b.determine_consensus_sequence(), 'ATCGAT')
+
+    def test_determine_consensus_sequence_c(self):
+        with self.assertRaises(ValueError):
+            # Should fail when determining consensus sequence given an indel
+            self.c.determine_consensus_sequence()
+        self.assertIn(self.c.determine_consensus_sequence([0]), 'ATCGAA')
+
+    def test_has_indel(self):
+        self.assertFalse(self.a.has_indel())
+        self.assertFalse(self.b.has_indel())
+        self.assertTrue(self.c.has_indel())
+
+    def test_construct_probe_a(self):
+        self.assertEqual(self.a.construct_probe(0, 4, [0,1,2,3,4], 0),
+                         ('ATCG', [0,1,2,3]))
+        self.assertEqual(self.a.construct_probe(0, 4, [0,1,2,3,4], 1),
+                         ('ATCG', [0,1,2,3,4]))
+        self.assertEqual(self.a.construct_probe(0, 4, [4], 1),
+                         ('AGCG', [4]))
+        self.assertIn(self.a.construct_probe(0, 4, [2,3], 0),
+                      [('ATCG', [2,3]), ('ACCG', [2,3])])
+        self.assertEqual(self.a.construct_probe(1, 4, [0,1,2,3,4], 0),
+                         ('TCGA', [0,1,2,3]))
+        self.assertEqual(self.a.construct_probe(2, 4, [0,1,2,3,4], 0),
+                         ('CGAA', [0,2,4]))
+        self.assertEqual(self.a.construct_probe(2, 4, [0,1,2,3,4], 1),
+                         ('CGAA', [0,1,2,3,4]))
+        self.assertEqual(self.a.construct_probe(2, 4, [0,1,2,3,4], 2),
+                         ('CGAA', [0,1,2,3,4]))
+        self.assertIn(self.a.construct_probe(2, 4, [0,1,2,3], 0),
+                      [('CGAA', [0,2]), ('CGAT', [1,3])])
+        self.assertIn(self.a.construct_probe(2, 4, [0,1,2,3], 1),
+                      [('CGAA', [0,1,2,3]), ('CGAT', [0,1,2,3])])
+
+    def test_construct_probe_b(self):
+        self.assertEqual(self.b.construct_probe(0, 4, [0,1,2,3,4], 0),
+                         ('ATCG', [0,2]))
+        self.assertEqual(self.b.construct_probe(0, 4, [0,1,2,3,4], 1),
+                         ('ATCG', [0,2]))
+        self.assertEqual(self.b.construct_probe(0, 4, [0,1,2,3,4], 2),
+                         ('ATCG', [0,1,2,3,4]))
+        self.assertEqual(self.b.construct_probe(2, 4, [0,1,2,3,4], 0),
+                         ('CGAA', [0]))
+        self.assertEqual(self.b.construct_probe(2, 4, [0,1,2,3,4], 1),
+                         ('CGAT', [0]))
+        self.assertEqual(self.b.construct_probe(2, 4, [0,1,2,3,4], 2),
+                         ('CGAT', [0,1,2,3]))
+        self.assertEqual(self.b.construct_probe(2, 4, [0,1,2,3,4], 3),
+                         ('CGAT', [0,1,2,3,4]))
+        self.assertEqual(self.b.construct_probe(2, 4, [2,4], 1),
+                         ('CGAC', []))
+        self.assertEqual(self.b.construct_probe(2, 4, [2,4], 2),
+                         ('CGAC', [2,4]))
+        self.assertIn(self.b.construct_probe(2, 4, [2,3,4], 2),
+                      [('CGAC', [2,4]), ('CGAT', [2,3])])
+        with self.assertRaises(alignment.CannotConstructProbeError):
+            # Should fail when 'N' is all that exists at a position
+            self.b.construct_probe(0, 4, [1,3,4], 0)
+
+    def test_construct_probe_c(self):
+        with self.assertRaises(alignment.CannotConstructProbeError):
+            # Should fail when alignment has an indel
+            self.c.construct_probe(0, 4, [0,1], 0)
