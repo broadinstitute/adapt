5af92b7f8888f8a2d5e2ec2000b2e908bbf05126
Hayden Metsky <hmetsky@gmail.com>
2019-01-26 15:20:11 -0500

    Add functions for downloading and parsing NCBI influenza genome database
    Most influenza genomes are stored and accessed in a separate database
    from the genome neighbors for the other species. This commit adds functions
    to ncbi_neighbors that access that database (via FTP) to construct Neighbor
    objects for influenza sequences.

--- dxguidedesign/prepare/ncbi_neighbors.py
+++ dxguidedesign/prepare/ncbi_neighbors.py
@@ -1,7 +1,10 @@
 """Utilities for working with genome neighbors (complete genomes) from NCBI.
 """
 
+import datetime
+import gzip
 import logging
+import re
 import tempfile
 import time
 import urllib.parse
@@ -45,6 +48,41 @@ def fetch_neighbors_table(taxid):
         line_rstrip = line.rstrip()
         if line_rstrip != '':
             yield line_rstrip
+
+
+def ncbi_influenza_genomes_url():
+    """Construct URL for downloading NCBI influenza genomes database.
+
+    Returns:
+        str representing download URL
+    """
+    url = 'ftp://ftp.ncbi.nih.gov/genomes/INFLUENZA/genomeset.dat.gz'
+    return url
+
+
+def fetch_influenza_genomes_table(species_name):
+    """Fetch influenza genome table from NCBI.
+
+    Args:
+        species_name: filter to keep only lines that contain this species
+            name
+
+    Yields:
+        lines, where each line is from the genome database table and
+        each line is a str
+    """
+    logger.debug(("Fetching table of influenza genomes for species %s") %
+        species_name)
+    species_name_lower = species_name.lower()
+
+    url = ncbi_influenza_genomes_url()
+    r = urllib.request.urlopen(url)
+    raw_data = gzip.GzipFile(fileobj=r).read()
+    for line in raw_data.decode('utf-8').split('\n'):
+        line_rstrip = line.rstrip()
+        if line_rstrip != '':
+            if species_name_lower in line_rstrip.lower():
+                yield line_rstrip
 
 
 def ncbi_download_url(accessions):
@@ -104,18 +142,20 @@ def fetch_fastas(accessions, batch_size=100, reqs_per_sec=2):
 class Neighbor:
     """Immutable representation of a genome neighbor."""
 
-    def __init__(self, acc, refseq_acc, hosts, lineage, tax_name, segment):
+    def __init__(self, acc, refseq_acc, hosts, lineage, tax_name, segment,
+            metadata={}):
         self.acc = acc
         self.refseq_acc = refseq_acc
         self.hosts = hosts
         self.lineage = lineage
         self.tax_name = tax_name
         self.segment = segment
+        self.metadata = metadata
 
     def _list_of_attrs(self):
         """List of all attributes (except the accession)."""
         return [self.refseq_acc, self.hosts, self.lineage, self.tax_name,
-            self.segment]
+            self.segment, self.metadata]
 
     def __eq__(self, other):
         return (self.acc == other.acc and
@@ -123,7 +163,8 @@ class Neighbor:
                 sorted(self.hosts) == sorted(other.hosts) and
                 self.lineage == other.lineage and
                 self.tax_name == other.tax_name and
-                self.segment == other.segment)
+                self.segment == other.segment,
+                self.metadata == other.metadata)
 
     def __repr__(self):
         return ';'.join('"' + str(s) + '"' for s in
@@ -179,3 +220,90 @@ def construct_neighbors(taxid):
     return neighbors
 
 
+def construct_influenza_genome_neighbors(taxid):
+    """Construct Neighbor objects for all influenza genomes
+
+    According to the README on NCBI's influenza database FTP site:
+    ```
+    The genomeset.dat file contains information for sequences of viruses with a
+    complete set of segments in full-length (or nearly
+    full-length).  Those of the same virus are grouped together (using an internal
+    group ID that is shown in the last column of the file) and separated by an
+    empty line from those of other viruses.
+    ```
+    fetch_influenza_genomes_table() returns genomeset.dat filtered for
+    a given species name.
+
+    According to that same README, the columns are:
+    ```
+    GenBank accession number[tab]Host[tab]Genome segment number or protein name
+    [tab]Subtype[tab]Country[tab]Year/month/date[tab]Sequence length
+    [tab]Virus name[tab]Age[tab]Gender
+    ```
+
+    Args:
+        taxid: taxonomic ID for an influenza species; must be influenza A
+            or B species
+
+    Returns:
+        list of Neighbor objects
+    """
+    logger.info(("Constructing a list of neighbors for influenza species "
+                 "with tax %d") % taxid)
+
+    influenza_species = {11320: 'Influenza A virus',
+                         11520: 'Influenza B virus'}
+    if taxid not in influenza_species:
+        raise ValueError(("Taxid (%d) must be for either influenza A or "
+                          "influenza B virus species") % taxid)
+    species_name = influenza_species[taxid]
+
+    influenza_lineages = {11320: ('Orthomyxoviridae', 'Alphainfluenzavirus',
+                                  'Influenza A virus'),
+                          11520: ('Orthomyxoviridae', 'Betainfluenzavirus',
+                                  'Influenza B virus')}
+    lineage = influenza_lineages[taxid]
+
+    # Construct a pattern to match years in a date (1000--2999)
+    year_p = re.compile('([1-2][0-9]{3})')
+
+    # Determine the current year
+    curr_year = int(datetime.datetime.now().year)
+
+    neighbors = []
+    for line in fetch_influenza_genomes_table(species_name):
+        if len(line.strip()) == 0:
+            continue
+
+        ls = line.split('\t')
+
+        acc = ls[0]
+        hosts = [ls[1]]
+        segment = ls[2]
+        subtype = ls[3]
+        country = ls[4]
+        date = ls[5]
+        seq_len = int(ls[6])
+        name = ls[7]
+
+        # Parse the year
+        year_m = year_p.match(date)
+        if year_m is None:
+            # No year available; skip the sequence
+            continue
+        year = int(year_m.group(1))
+        if year > curr_year:
+            # This year is in the future (probably a typo); skip the sequence
+            continue
+
+        # Construct dict of metadata
+        metadata = {'subtype': subtype, 'country': country, 'year': year,
+                    'seq_len': seq_len}
+
+        # Leave the refseq_acc as None because, in the influenza database,
+        # sequences are not assigned a particular RefSeq
+        neighbor = Neighbor(acc, None, hosts, lineage, name, segment,
+            metadata=metadata)
+        neighbors += [neighbor]
+
+    return neighbors
