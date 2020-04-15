a44fe3db20361b0b4acec225d8a12654a7d65ec8
Hayden Metsky <hmetsky@gmail.com>
2020-04-15 10:06:49 -0400

    Use serialized models directly for activity prediction
    This commit considerably rewrites the predict_activity module.
    Previously it used a crude approach to read models from another
    directory. Now, this allows the module to read models saved in
    TensorFlow's SavedModel format. It accepts paths to these models and
    uses them for prediction.
    
    This commit also changes the module to perform both classification and
    regression -- i.e., it uses two models, one for each. For a guide-target
    pair to be evaluated as 'True' (i.e., evaluate() returns True) it must
    be both classified to be active and then determined to be highly active
    based on regression.

--- adapt/utils/predict_activity.py
+++ adapt/utils/predict_activity.py
@@ -1,44 +1,273 @@
 """Load and use a model to predict activity of a guide-target pair.
-
-The function construct_predictor() has a crude and temporary approach, with
-a hard-coded path to a separate directory from which to load modules that
-enable prediction. That implementation is currently commented out.
 """
 
 import os
-import pickle
-import sys
+
+# Disable some (but apparently not all) INFO and WARNING messages
+# from TensorFlow; must come before the TensorFlow import
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
+
+import numpy as np
+import tensorflow as tf
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
 
-# Use the top ~25% of guide-target pairs as the default activity threshold
-DEFAULT_ACTIVITY_THRES = -0.85
+# Define function for creating a one-hot encoding from
+# nucleotide sequence
+FASTA_CODES = {'A': set(('A')),
+               'T': set(('T')),
+               'C': set(('C')),
+               'G': set(('G')),
+               'K': set(('G', 'T')),
+               'M': set(('A', 'C')),
+               'R': set(('A', 'G')),
+               'Y': set(('C', 'T')),
+               'S': set(('C', 'G')),
+               'W': set(('A', 'T')),
+               'B': set(('C', 'G', 'T')),
+               'V': set(('A', 'C', 'G')),
+               'H': set(('A', 'C', 'T')),
+               'D': set(('A', 'G', 'T')),
+               'N': set(('A', 'T', 'C', 'G'))}
+onehot_idx = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+def onehot(b):
+    # One-hot encoding of base b
+    real_bases = FASTA_CODES[b]
+    v = [0, 0, 0, 0]
+    for b_real in real_bases:
+        assert b_real in onehot_idx.keys()
+        v[onehot_idx[b_real]] = 1.0 / len(real_bases)
+    return v
 
 
 class Predictor:
-    """This calls the activity model and memoizes results.
+    """This calls the activity models and memoizes results.
     """
 
-    def __init__(self, model, pred_from_nt_fn, activity_thres=None):
+    def __init__(self, classification_model_path, regression_model_path,
+            classification_threshold=None,
+            regression_threshold=None):
         """
         Args:
-            model: model object with a call() function
-            pred_from_nt_fn: function that accepts list of guide-target pairs
-                (in nucleotide space) and outputs predicted activities
-            activity_thres: call predicted activity >= this threshold
-                to be positive; if None, use default
+            classification_model_path: path to serialized classification model
+            regression_model_path: path to serialized regression model
+            classification_threshold: call guide-target active when
+                classifier prediction is >= this threshold (in [0,1]); if
+                None, use default threshold with model
+            regression_threshold: call guide-target highly active
+                when regression prediction (on active data points) is
+                >= this threshold (in [0,4]); if None, use default
+                threshold with model
         """
-        self.model = model
-        self.pred_from_nt_fn = pred_from_nt_fn
-        if activity_thres is None:
-            activity_thres = DEFAULT_ACTIVITY_THRES
-        self.activity_thres = activity_thres
-        self.context_nt = model.context_nt
+        # Load classification and regression models
+        self.classification_model = tf.keras.models.load_model(
+                classification_model_path)
+        self.regression_model = tf.keras.models.load_model(
+                regression_model_path)
+
+        # Activity yalues are >= -4 (and most, but not all, are < 0); likewise,
+        # outputs of the regression model should be >= -4 (though some
+        # may be < -4, i.e., less than any training data value)
+        # Shift up regression predictions to be >= 0
+        self.regression_lower_bound = 0.0
+        self.regression_shift = 4.0
+
+        # Load context_nt; this should be the same for the classification
+        # and regression models
+        classification_context_nt_path = os.path.join(
+                classification_model_path, 'assets.extra/context_nt.arg')
+        regression_context_nt_path = os.path.join(
+                regression_model_path, 'assets.extra/context_nt.arg')
+        if not os.path.isfile(classification_context_nt_path):
+            raise Exception(("Unknown context_nt for classification model; "
+                "the model should have a assets.extra/context_nt.arg file"))
+        if not os.path.isfile(regression_context_nt_path):
+            raise Exception(("Unknown context_nt for regression model; "
+                "the model should have a assets.extra/context_nt.arg file"))
+        with open(classification_context_nt_path) as f:
+            classification_context_nt = int(f.readline().strip())
+        with open(regression_context_nt_path) as f:
+            regression_context_nt = int(f.readline().strip())
+        if classification_context_nt != regression_context_nt:
+            raise Exception(("Classification and regression models should "
+                "have been trained with the same context_nt, but they differ"))
+        self.context_nt = classification_context_nt
+
+        # Read classification and regression thresholds
+        # The classification threshold decides which guide-target pairs are
+        # active, and the regression threshold (trained only on active pairs)
+        # decides which are highly active
+        if classification_threshold is None:
+            # Read default threshold
+            classification_default_threshold_path = os.path.join(
+                    classification_model_path,
+                    'assets.extra/default_threshold.arg')
+            with open(classification_default_threshold_path) as f:
+                classification_threshold = float(f.readline().strip())
+            assert 0 <= classification_threshold <= 1
+        else:
+            if classification_threshold < 0 or classification_threshold > 1:
+                raise ValueError(("Classification threshold should be "
+                    "in [0,1]"))
+        self.classification_threshold = classification_threshold
+        if regression_threshold is None:
+            # Read default threshold
+            regression_default_threshold_path = os.path.join(
+                    regression_model_path,
+                    'assets.extra/default_threshold.arg')
+            with open(regression_default_threshold_path) as f:
+                regression_threshold = float(f.readline().strip())
+            # Add to the regression threshold the shift, as the default
+            # threshold (in contrast to a specified threshold) is in
+            # the range of the direct model outputs
+            regression_threshold += self.regression_shift
+            assert regression_threshold > self.regression_lower_bound
+        else:
+            if regression_threshold < 0:
+                raise ValueError(("Regression threshold should be >= 0"))
+        self.regression_threshold = regression_threshold
 
         # Memoize evaluations, organized by guide start position:
         #   {guide start: {pair: result of evaluation}}
         self._memoized_evaluations = {}
+
+    def _model_input_from_nt(self, pairs):
+        """Create one-hot input to models from nucleotide sequence.
+
+        Args:
+            pairs: list of tuples (target with context, guide)
+
+        Returns:
+            list of one-hot encodings for each pair
+        """
+        if len(pairs) == 0:
+            return []
+
+        l = 2*self.context_nt + len(pairs[0][1])
+        x = np.empty((len(pairs), l, 8), dtype='f')
+        for i, (target_with_context, guide) in enumerate(pairs):
+            assert len(target_with_context) == 2*self.context_nt + len(guide)
+
+            # Determine one-hot encodings -- i.e., an input vector
+            input_vec = []
+            for pos in range(self.context_nt):
+                v_target = onehot(target_with_context[pos])
+                v_guide = [0, 0, 0, 0]
+                input_vec += [v_target + v_guide]
+            for pos in range(len(guide)):
+                v_target = onehot(target_with_context[self.context_nt + pos])
+                v_guide = onehot(guide[pos])
+                input_vec += [v_target + v_guide]
+            for pos in range(self.context_nt):
+                v_target = onehot(target_with_context[self.context_nt + len(guide) + pos])
+                v_guide = [0, 0, 0, 0]
+                input_vec += [v_target + v_guide]
+            input_vec = np.array(input_vec, dtype='f')
+            x[i] = input_vec
+        return x
+
+    def _predict_from_onehot(self, model, pairs_onehot):
+        """Predict activity, from one-hot encoded nucleotide sequence,
+        using a model.
+
+        Args:
+            model: model object (serialized SavedModel) that has a
+                call() function
+            pairs_onehot: list of one-hot encoded pairs of target and guide
+
+        Returns:
+            list of outputs (float) directly from model, with one
+            value per item in pairs
+        """
+        pred_activity = model.call(pairs_onehot, training=False)
+        pred_activity = [p[0] for p in pred_activity.numpy()]
+        return pred_activity
+
+    def _classify(self, pairs_onehot):
+        """Run classification model.
+
+        Args:
+            pairs_onehot: list of one-hot encoded pairs of target and guide
+
+        Returns:
+            list of False or True giving result of whether each pair is active
+            based on classification, after making decision based on
+            self.classification_threshold
+        """
+        if len(pairs_onehot) == 0:
+            return []
+        pred_classification_score = self._predict_from_onehot(
+                self.classification_model, pairs_onehot)
+        return [bool(p >= self.classification_threshold)
+                for p in pred_classification_score]
+
+    def _regress(self, pairs_onehot):
+        """Run regression model.
+
+        Args:
+            pairs_onehot: list of one-hot encoded pairs of target and guide
+
+        Returns:
+            list of False or True giving result of whether each pair is highly
+            active based on regression, after making decision based on
+            self.regression_threshold
+        """
+        if len(pairs_onehot) == 0:
+            return []
+        pred_regression_score = self._predict_from_onehot(
+                self.regression_model, pairs_onehot)
+
+        # The regression model is trained (and outputs) negative values;
+        # shift these up to be positive and enforce a lower-bound
+        pred_regression_score = [max(self.regression_lower_bound,
+            p + self.regression_shift) for p in pred_regression_score]
+
+        return [bool(p >= self.regression_threshold)
+                for p in pred_regression_score]
+
+    def _determine_highly_active(self, pairs):
+        """Run classification model and regression model.
+
+        This runs classification on all pairs, and then regression
+        on just the ones decided to be active.
+
+        Args:
+            pairs: list of tuples (target with context, guide)
+
+        Returns:
+            list of False or True giving result of whether a guide-target
+            pair is predicted to be *both* active and highly active
+        """
+        # Create one-hot encoding of pairs
+        pairs_onehot = self._model_input_from_nt(pairs)
+
+        # Run classification on all pairs
+        classification_results = self._classify(pairs_onehot)
+
+        # Pull out pairs that are active
+        pairs_onehot_active = [pair_onehot
+                for pair_onehot, p in zip(pairs_onehot, classification_results)
+                if p is True]
+
+        # Run regression on the active pairs
+        regression_results = self._regress(pairs_onehot_active)
+
+        # Merge the results of classification and regression
+        results = [None for _ in range(len(pairs))]
+        j = 0
+        for i in range(len(pairs)):
+            if classification_results[i] is False:
+                # Classified inactive
+                results[i] = False
+            else:
+                # Classified active, so use regression results
+                # that decide if highly active
+                results[i] = regression_results[j]
+                j += 1
+        assert j == len(regression_results)
+
+        return results
 
     def evaluate(self, start_pos, pairs):
         """Evaluate guide-target pairs, with memoized results.
@@ -48,7 +277,7 @@ class Predictor:
             pairs: list of tuples (target with context, guide)
 
         Returns:
-            results of self.call() on these pairs
+            results of self._determine_highly_active() on these pairs
         """
         if start_pos not in self._memoized_evaluations:
             self._memoized_evaluations[start_pos] = {}
@@ -57,7 +286,7 @@ class Predictor:
         # Determine which pairs do not have memoized results, and call
         # these
         unique_pairs_to_evaluate = [pair for pair in set(pairs) if pair not in mem]
-        evaluations = self.call(unique_pairs_to_evaluate)
+        evaluations = self._determine_highly_active(unique_pairs_to_evaluate)
         for pair, e in zip(unique_pairs_to_evaluate, evaluations):
             mem[pair] = e
 
@@ -72,59 +301,3 @@ class Predictor:
         if start_pos in self._memoized_evaluations:
             del self._memoized_evaluations[start_pos]
 
-    def call(self, pairs):
-        """Determine whether or not a guide-target pair has sufficient
-        activity. 
-
-        Args:
-            pairs: list of tuples (target with context, guide)
-
-        Returns:
-            list of False/True indicating whether the predicted activity of
-            each guide-target pair exceeds self.activity_thres
-        """
-        if len(pairs) == 0:
-            return []
-        pred_activity = self.pred_from_nt_fn(pairs)
-        return [pa >= self.activity_thres for pa in pred_activity]
-
-
-def construct_predictor(model_path, context_nt=10, activity_thres=None):
-    """Construct a Predictor object.
-
-    Args:
-        model_path: path to saved model parameters and weights
-        context_nt: nt of context to use on each side of target
-        activity_thres: call predicted activity >= this threshold to be
-            positive; if None, use default
-
-    Returns:
-        Predictor object
-    """
-    raise NotImplementedError(("Not yet implemented in a general way; "
-        "manually edit this function to use"))
-
-    """
-    load_path_params = os.path.join(model_path,
-            'model.params.pkl')
-    with open(load_path_params, 'rb') as f:
-        saved_params = pickle.load(f)
-    params = {'dataset': 'cas13', 'cas13_subset': 'exp-and-pos',
-            'cas13_regress_only_on_active': True, 'context_nt': context_nt}
-    for k, v in saved_params.items():
-        params[k] = v
-
-    # Load predictor module from separate directory
-    sys.path.append(os.path.join(os.path.expanduser("~"), "adapt-seq-design"))
-    import predictor
-
-    predictor.set_seed(1)
-
-    # Load model and define function for predicting from nucleotide sequence
-    model = predictor.load_model_for_cas13_regression_on_active(
-            model_path, params)
-    def pred_from_nt(pairs):
-        return predictor.pred_from_nt(model, pairs)
-
-    return Predictor(model, pred_from_nt, activity_thres=activity_thres)
-    """
