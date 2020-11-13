fc8c5c479bf167f1e1d41703da5b7c367e9ec949
Hayden Metsky <hmetsky@gmail.com>
2020-11-13 12:50:31 -0500

    Add note to help message that activity predictions are still memoized

--- bin/design.py
+++ bin/design.py
@@ -997,7 +997,8 @@ if __name__ == "__main__":
               "specificity queries. This can be helpful for benchmarking "
               "the improvement of memoization, or if there is reason "
               "to believe memoization will slow the search (e.g., "
-              "if possible amplicons rarely overlap)"))
+              "if possible amplicons rarely overlap). Note that activity "
+              "predictions are still memoized.))
 
     # Log levels
     base_subparser.add_argument("--debug",
